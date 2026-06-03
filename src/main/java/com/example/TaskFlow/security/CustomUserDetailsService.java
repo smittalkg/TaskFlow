fd300e8f7d8d76932dc3586c23f6fd4e38e3eb56
Sanchit <sanchitmittal18@gmail.com>
2026-06-03 15:18:42 +0530

    authentication filter

---
+++ src/main/java/com/example/TaskFlow/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.TaskFlow.security;
+
+import com.example.TaskFlow.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> User
+                        .withUsername(user.getEmail())
+                        .password(user.getPassword())
+                        .roles("USER")
+                        .build()
+                )
+                .orElseThrow(() -> new UsernameNotFoundException("User not found" + email));
+    }
+}
